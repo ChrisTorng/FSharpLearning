6ce09c732995db460d589575baab23530bd45d82
ChrisTorng <christorng@hotmail.com>
2021-11-24 08:35:24 +0800

    FarmerSample

---
+++ FarmerSample/Program.fs
@@ -0,0 +1,31 @@
+﻿open Farmer
+open Farmer.Builders
+
+// Create a storage account with a container
+let myStorageAccount = storageAccount {
+    name "myteststorage"
+    add_public_container "mycontainer"
+}
+
+// Create a web app with application insights that's connected to the storage account.
+let myWebApp = webApp {
+    name "myTestWebApp"
+    setting "storageKey" myStorageAccount.Key
+}
+
+// Create an ARM template
+let deployment = arm {
+    location Location.NorthEurope
+    add_resources [
+        myStorageAccount
+        myWebApp
+    ]
+}
+
+// Deploy it to Azure!
+deployment
+|> Writer.quickWrite "myResourceGroup"
+
+// deployment
+// |> Deploy.execute "myResourceGroup" Deploy.NoParameters
+// |> ignore
